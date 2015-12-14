2741a7032b3ac28fc3071f01e4301481ec835aaa
sisikopf <dimko12@gmail.com>
2015-12-14 17:17:04 +0300

    Added libgdx

---
+++ desktop/src/com/vsu/csf/arkanoid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vsu.csf.arkanoid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vsu.csf.arkanoid.ArkanoidGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArkanoidGame(), config);
+	}
+}
